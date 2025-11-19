b4acad6ab80379002a8378e4838f78e1746da276
Sho Sakuma <me@m1sk9.dev>
2025-11-19 21:20:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RuneCore
+
+Rune of Mel におけるコアプラグイン．
+
+貢献に関するガイドは [こちら](.github/CONTRIBUTING.md)を参照してください．
---
+++ src/main/kotlin/dev/m1sk9/runeCore/RuneCore.kt
@@ -0,0 +1,14 @@
+package dev.m1sk9.runeCore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RuneCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
